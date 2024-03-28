5bab163839ecb94ea108f4d933a9b3a5e8912a77
David Frey <david.frey@charter.com>
2024-03-28 15:00:44 -0600

    build out of conversion page

--- rocketcalc.py
+++ rocketcalc.py
@@ -1,3 +1,12 @@
 import streamlit as st
+import pyperclip
 
-st.write("Rocket Calculator")
+st.title("Rocket Calculator")
+st.subheader("Millimeters to Inches")
+
+mm = st.text_input(label="Enter millimeters", value="")
+if st.button("Convert") and mm is not None:
+    inches = round(float(mm)/25.4,4)
+
+    st.code(inches, language="python")
+
